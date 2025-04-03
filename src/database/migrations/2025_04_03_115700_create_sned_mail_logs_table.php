8b615462a8c84c8aad2727dcb5679de578d9e3d9
okajima ryuya <okajima@altana.co.jp>
2025-04-03 15:15:40 +0900

    メール送信ログテーブル

---
+++ src/database/migrations/2025_04_03_115700_create_sned_mail_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sned_mail_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('from_email')->comment('送信元メールアドレス');
+            $table->string('to_email')->comment('送信先メールアドレス');
+            $table->string('subject')->comment('メールの件名');
+            $table->string('body')->comment('本文');
+            $table->unsignedTinyInteger('status')->comment('メール送信ステータス');
+            $table->string('error_message')->comment('メール送信失敗時のメッセージ');
+            $table->dateTime('send_time_at')->comment('メール送信日時');
+            $table->dateTime('created_at')->comment('作成日時');
+        });
+        DB::statement("ALTER TABLE sned_mail_logs COMMENT = '送信メールログ'");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('associate_users');
+    }
+};
