7c612a445ed15f4acb0349de8afc94d61c9d92c6
okajima ryuya <okajima@altana.co.jp>
2025-04-10 17:46:10 +0900

    モック更新

--- frontend/js/user/index.js
+++ frontend/js/user/index.js
@@ -1,24 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // ハンバーガーメニュー
+    hamburgerMenu();
+    // スライドショー
+    slideShow();
+});
+
+function hamburgerMenu() {
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav');
   
     hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('active');
-      nav.classList.toggle('active');
-  
-      // アクセシビリティ対応
-      const isOpen = hamburger.classList.contains('active');
-      hamburger.setAttribute('aria-expanded', isOpen);
-      nav.setAttribute('aria-hidden', !isOpen);
+        hamburger.classList.toggle('active');
+        nav.classList.toggle('active');
+    
+        // アクセシビリティ対応
+        const isOpen = hamburger.classList.contains('active');
+        hamburger.setAttribute('aria-expanded', isOpen);
+        nav.setAttribute('aria-hidden', !isOpen);
     });
   
     // メニューの外側をクリックした時の処理
     document.addEventListener('click', (e) => {
-      if (!e.target.closest('.nav') && !e.target.closest('.hamburger') && nav.classList.contains('active')) {
-        hamburger.classList.remove('active');
-        nav.classList.remove('active');
-        hamburger.setAttribute('aria-expanded', false);
-        nav.setAttribute('aria-hidden', true);
-      }
+        if (!e.target.closest('.nav') && !e.target.closest('.hamburger') && nav.classList.contains('active')) {
+            hamburger.classList.remove('active');
+            nav.classList.remove('active');
+            hamburger.setAttribute('aria-expanded', false);
+            nav.setAttribute('aria-hidden', true);
+        }
     });
-});
+}
+
+function slideShow() {
+
+}
