ab776186a42cb0b2ab2a55cdc4d0f016dd10c358
okajima ryuya <okajima@altana.co.jp>
2025-04-16 13:15:30 +0900

    管理ユーザ一覧　雛形

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('admin.')->group(function () {
-    Route::controller(HomeController::class)->group(function() {
+    Route::prefix('/')->controller(HomeController::class)->group(function() {
+        Route::get('/', 'index')->name('index');
+    });
+    Route::prefix('/user')->controller(UserController::class)->group(function() {
         Route::get('/', 'index')->name('index');
     });
 });
