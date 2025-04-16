2eb55acf6eb59bc7c4336b98b8950c85339f5912
okajima ryuya <okajima@altana.co.jp>
2025-04-16 11:50:07 +0900

    blade修正

--- src/resources/views/user/login/index.blade.php
+++ src/resources/views/user/login/index.blade.php
@@ -1,5 +1,21 @@
-<x-user.layout :title="$title" :description="$description">
-    <x-user.header />
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="{{ $meta->description }}">
+    <meta name="author" content="okajima">
+    <meta http-equiv="Content-Language" content="ja">
+    <title>{{ $meta->title }}</title>
+    <link rel="shortcut icon" href="{{ asset('/img/icon/favicon.png') }}">
+    <link rel="stylesheet" href="{{ asset('/css/user/index.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <script src="{{ asset('/js/index.js') }}" defer></script>
+</head>
+<body>
+<x-user.header />
     <main>
         <x-common.flash_message name="message" />
         <h2 class="gest-title">アカウントログイン</h2>
@@ -31,4 +47,5 @@
         </section>
     </main>
     <x-user.footer />
-</x-admin.layout>
+</body>
+</html>
