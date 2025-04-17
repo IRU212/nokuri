3e5c3999f9a56d30bd07bf4534bf43edbe25994a
okajima ryuya <okajima@altana.co.jp>
2025-04-17 11:18:52 +0900

    seeder 修正

--- src/database/seeders/Production/ProductionWorkOutSeeder.php
+++ src/database/seeders/Production/ProductionWorkOutSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders\Production;
 
 use App\Models\WorkOut;
+use App\Services\CsvService;
 use Illuminate\Database\Seeder;
 
 final class ProductionWorkOutSeeder extends Seeder
@@ -12,22 +13,16 @@ final class ProductionWorkOutSeeder extends Seeder
      */
     public function run(): void
     {
-        $params = [
-            ['id' => 1, 'name' => 'ダンベルショルダープレス', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 2, 'name' => 'ダンベルサイドレイズ', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 3, 'name' => 'ダンベルフロントレイズ', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 4, 'name' => 'ダンベルリアレイズ', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 5, 'name' => 'バーベルショルダープレス', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 6, 'name' => 'バーベルアップライトロウ', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 7, 'name' => 'バーベルフロントレイズ', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 8, 'name' => 'ケーブルショルダープレス', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 9, 'name' => 'ケーブルサイドレイズ', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 10, 'name' => 'ケーブルリアレイズ', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 11, 'name' => 'ケーブルフロントレイズ', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 12, 'name' => 'ショルダープレスマシン', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 13, 'name' => 'ラテラルレイズマシン', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-            ['id' => 14, 'name' => 'ペクトラルフライマシン', 'is_exercise_equipment' => true, 'is_bodyweight' => false],
-        ];
-        WorkOut::upsert($params, ['id'], ['updated_at']);
+        $csv_sevice = new CsvService(storage_path('seeders/data/work_out.csv'));
+        $csv_data_list_arr = $csv_sevice->convertFileToArray();
+
+        foreach ($csv_data_list_arr as $csv_data_arr) {
+            WorkOut::create([
+                'id'                    => $csv_data_arr[0],
+                'name'                  => $csv_data_arr[1],
+                'is_exercise_equipment' => $csv_data_arr[2],
+                'is_bodyweight'         => $csv_data_arr[3],
+            ]);
+        }
     }
 }
