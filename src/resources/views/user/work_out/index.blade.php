924c37248d13a4bebaa19afca65c49742203cad5
okajima ryuya <okajima@altana.co.jp>
2025-04-18 11:26:58 +0900

    修正

--- src/resources/views/user/work_out/index.blade.php
+++ src/resources/views/user/work_out/index.blade.php
@@ -5,7 +5,7 @@
             <h3 class="title">筋トレメニュー一覧</h3>
             <ul class="work-out__group">
                 @foreach ($work_outs as $loop => $work_out)
-                    <li class="work-out__card @if($loop->iteration === 2 || $loop->iteration - 2 % 3 === 0) side-auto @endif">
+                    <li class="work-out__card @if(($loop->iteration - 2) % 3 === 0) side-auto @endif">
                         <div class="work-out__card__title">「{{ $work_out->name }}」</div>
                         <div class="work-out__card__description">
                             ベンチプレスとなります。ベンチプレスとなります。ベンチプレスとなります。ベンチプレスとなります。
