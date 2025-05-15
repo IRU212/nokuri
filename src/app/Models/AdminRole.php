d83ba24cf9b122544b0c7e2b985df8d0b07e54d5
okajima ryuya <okajima@altana.co.jp>
2025-05-15 14:19:30 +0900

    修正

--- src/app/Models/AdminRole.php
+++ src/app/Models/AdminRole.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\DisplayScope;
 use Illuminate\Database\Eloquent\Model;
 
 class AdminRole extends Model
 {
+    use DisplayScope;
+
     /**
      * 複数代入可能な属性
      *
